1c0fc5bedf90fb93856fa232421fabe9f0a60718
Samvid <samvid25@yahoo.com>
2017-09-17 17:07:32 +0530

    Add files

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function bold()
+{
+    document.execCommand('bold');
+}
+
+function underline() {
+    document.execCommand('underline');
+}
+
+function italic() {
+    document.execCommand('italic');
+}
+
+function copy()
+{
+    document.execCommand('copy');
+}
+
+function list()
+{
+    document.execCommand('insertUnorderedList');
+}
+
+function justifyLeft()
+{
+    document.execCommand('justifyLeft');
+}
+
+function justifyRight() {
+    document.execCommand('justifyRight');
+}
