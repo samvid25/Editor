64da6bc1840d522934174da96048b510fbc0db8c
Samvid <samvid25@yahoo.com>
2017-09-18 22:12:09 +0530

    Add save as and open features

--- js/script.js
+++ js/script.js
@@ -85,3 +85,41 @@ function createLink()
    // var a = prompt('Enter the URL');
     document.execCommand('createLink', a);
 }*/
+
+function save()
+{
+    var text = $("#textarea").html();
+    var file = new File([text], "file.edit", { type: "text/html;charset=utf-8" })
+    saveAs(file);
+}
+
+
+var fs = require('fs');
+var remote = require('electron');
+var dialog = remote.remote.dialog;
+var path = require('path');
+console.log(dialog);
+
+function openfile() {
+    console.log("TEST");
+
+
+    dialog.showOpenDialog({
+        properties: ['openFile']
+    }, (fileNames) => {
+        // fileNames is an array that contains all the selected
+        if (fileNames === undefined) {
+            console.log("No file selected");
+            return;
+        }
+        fs.readFile(fileNames[0], 'utf-8', (err, data) => {
+            if (err) {
+                alert("An error ocurred reading the file :" + err.message);
+                return;
+            }
+
+            // Change how to handle the file content
+            $("#textarea").html(data);
+        });
+    });
+}
