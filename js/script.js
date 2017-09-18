207b189b08467c7869094d52d570534e90d35786
Samvid <samvid25@yahoo.com>
2017-09-18 18:51:46 +0530

    Add icons and more options

--- js/script.js
+++ js/script.js
@@ -16,9 +16,17 @@ function copy()
     document.execCommand('copy');
 }
 
-function list()
+function cut() {
+    document.execCommand('cut');
+}
+
+function unorderedList()
 {
     document.execCommand('insertUnorderedList');
+}
+
+function orderedList() {
+    document.execCommand('insertOrderedList');
 }
 
 function justifyLeft()
@@ -29,3 +37,51 @@ function justifyLeft()
 function justifyRight() {
     document.execCommand('justifyRight');
 }
+
+function justifyCenter() {
+    document.execCommand('justifyCenter');
+}
+
+function justifyAll()
+{
+    document.execCommand('justifyFull');
+}
+
+function strikeThrough()
+{
+    document.execCommand('strikeThrough');
+}
+
+function subscript() {
+    document.execCommand('subscript');
+}
+
+function superscript() {
+    document.execCommand('superscript');
+}
+
+function undo() {
+    document.execCommand('undo');
+}
+
+function redo() {
+    document.execCommand('redo');
+}
+
+function selectAll()
+{
+    document.execCommand('selectAll');
+}
+
+/* TO DO
+function highlight()
+{   //stylewithCSS reqd
+    document.execCommand('hiliteColor');
+}
+
+function createLink()
+{
+    //use third party lib for prompt
+   // var a = prompt('Enter the URL');
+    document.execCommand('createLink', a);
+}*/
